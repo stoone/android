af1f7e97099eaf7555b05bde911a3f4220de8c78
stoone <nxz0010@yeah.net>
2014-08-25 18:20:11 +0800

    android
    learning android. Sample code

---
+++ GeoQuiz/src/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -0,0 +1,177 @@
+package com.bignerdranch.android.geoquiz;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class QuizActivity extends ActionBarActivity {
+	
+	private int mCurrentIndex = 0;
+	private Button mTrueButton;
+	private Button mFalseButton;
+	private Button mNextButton;
+	private Button mCheatButton;
+	private TextView mQuestionTextView;
+	private static final String TAG = "QuizActivity";
+	private static final String KEY_INDEX = "index";
+	
+	
+	private void checkAnswer(boolean userPressedTrue)
+	{
+		boolean answerIsTrue = mQuestionBank[mCurrentIndex].isTrueQuestion();
+		
+		int messageResId = 0;
+		if (userPressedTrue == answerIsTrue)
+		{
+			messageResId = R.string.correct_toast;
+		} 
+		else
+		{
+			messageResId = R.string.incorrect_toast;
+		}
+		
+		Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
+	}
+	
+	private TrueFalse[] mQuestionBank = new TrueFalse[]{
+			new TrueFalse(R.string.question_oceans, true),
+			new TrueFalse(R.string.question_mideast,  false),
+			new TrueFalse(R.string.question_africa, false),
+			new TrueFalse(R.string.question_americas, true),
+			new TrueFalse(R.string.question_asia, true),
+	};
+	
+	
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+        
+        if (savedInstanceState != null)
+        	mCurrentIndex = savedInstanceState.getInt(KEY_INDEX);
+        
+        Log.d(TAG, "onCreate(Bundle) called");
+        mQuestionTextView = (TextView)findViewById(R.id.question_text_view);
+        mQuestionTextView.setOnClickListener(new View.OnClickListener()
+        {
+        	public void onClick(View v)
+        	{
+				mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+				updateQuestion();
+        	}
+        });
+        
+        mCheatButton = (Button)findViewById(R.id.cheat_button);
+        mCheatButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent i = new Intent(QuizActivity.this, CheatActivity.class);
+				boolean answerIsTrue = mQuestionBank[mCurrentIndex].isTrueQuestion();
+				i.putExtra(CheatActivity.EXTRA_ANSWER_IS_TRUE, answerIsTrue);
+				startActivity(i);
+			}
+		});
+        
+        updateQuestion();
+        
+        mTrueButton = (Button)findViewById(R.id.true_button);
+		mTrueButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				checkAnswer(true);
+			}
+		});
+			
+		mFalseButton = (Button)findViewById(R.id.false_button);
+		mFalseButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				checkAnswer(false);
+			}
+		});
+		
+		mNextButton = (Button)findViewById(R.id.next_button);
+		mNextButton.setOnClickListener(new View.OnClickListener()
+		{
+			
+			@Override
+			public void onClick(View v)
+			{
+				// TODO Auto-generated method stub
+				mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+				updateQuestion();
+			}
+		});
+    }
+
+    @Override
+    public void onStart()
+    {
+    	super.onStart();
+    	Log.d(TAG, "onStart() called");
+    }
+    
+    @Override
+    public void onPause()
+    {
+    	super.onPause();
+    	Log.d(TAG, "OnPause() called");
+    }
+    
+    @Override
+    public void onResume()
+    {
+    	super.onResume();
+    	Log.d(TAG, "onResume() called");
+    }
+    
+    @Override
+    public void onStop()
+    {
+    	super.onStop();
+    	Log.d(TAG, "onStop() called");
+    }
+    
+    @Override
+    public void onDestroy()
+    {
+    	super.onDestroy();
+    	Log.d(TAG, "onDestroy() called");
+    }
+    
+    private void updateQuestion()
+    {
+    	int question = mQuestionBank[mCurrentIndex].getQuestion();
+    	mQuestionTextView.setText(question);
+    }
+    
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState)
+    {
+    	super.onSaveInstanceState(savedInstanceState);
+    	Log.d(TAG, "onSaveInstanceState");
+    	savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.quiz, menu);
+        return true;
+    }
+
+}
